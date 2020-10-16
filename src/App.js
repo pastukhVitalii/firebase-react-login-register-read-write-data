ed3509d1cbf83cacc3993dd407a1bd4f2fefa8b2
pastukhVitalii <pastukh.v.7@gmail.com>
2020-10-16 22:35:04 +0300

    add fire base login, register, read, write data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import * as firebase from 'firebase';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            hasAccount: false,
+            name: '',
+            key: '',
+            value: ''
+        }
+    }
+
+    componentDidMount() {
+        //read db
+        const db = firebase.database();
+        const name = db.ref('name');
+        name.on('value', (elem) => {
+            this.setState({name: elem.val()});
+        });
+        console.log(db)
+    }
+
+    handleChange = ({target: {value, id}}) => {
+        this.setState({
+            [id]: value,
+        })
+    }
+
+    createAcount = () => {
+        const {email, password} = this.state;
+        /*firebase.auth().createUserWithEmailAndPassword(email, password)
+            .catch(error => console.log(error) )*/
+
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(() => {
+                this.setState({hasAccount: true});
+            })
+            .catch(error => console.log(error))
+    }
+
+    sendData = () => {
+        const {key, value} = this.state;
+        const db = firebase.database();
+        db.ref(key).push(value);
+        alert('your data written to db');
+    };
+
+    render() {
+        const {hasAccount, name} = this.state;
+        return (
+            <div>
+                {hasAccount ?
+                    <div>
+                        <input type="text"
+                               id='key'
+                               placeholder='enter key'
+                               onChange={this.handleChange}/>
+                        <input type="text"
+                               id='value'
+                               placeholder='enter value'
+                               onChange={this.handleChange}/>
+                        <input type="submit"
+                               onClick={this.sendData}/>
+                    </div> :
+                    <div className="login-block">
+                        <input type="text"
+                               placeholder='email'
+                               id='email'
+                               onChange={this.handleChange}/>
+                        <input type="password"
+                               placeholder='password'
+                               id='password'
+                               onChange={this.handleChange}/>
+                        <input type="submit"
+                               onClick={this.createAcount}/>
+                    </div>
+                }
+            </div>
+        )
+    }
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBdgJa29KDP6zolVowa9sVesFu16Xu_3uQ",
+    authDomain: "fir-react-first-project.firebaseapp.com",
+    databaseURL: "https://fir-react-first-project.firebaseio.com",
+    projectId: "fir-react-first-project",
+    storageBucket: "fir-react-first-project.appspot.com",
+    messagingSenderId: "988238844126",
+    appId: "1:988238844126:web:c8c98ed2b48a7466cdf24f",
+    measurementId: "G-DBJZSJD9SQ"
+}
+
+firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(
   <React.StrictMode>
